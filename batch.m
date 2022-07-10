97a3f7a1c1885c0e57d36b46388665aef5c54fe1
TOMORI233 <ihatewo@qq.com>
2022-07-10 18:53:13 +0800

    update 20220710 18:53 by xhx

--- batch.m
+++ batch.m
@@ -4,7 +4,6 @@ addpath(genpath(fileparts(mfilename("fullpath"))));
 
 %% Parameter settings
 ROOTPATH = "D:\Education\Lab\Projects\EEG\Data\CDT\20220708";
-window = [0, 2000];
 th = 0.2;
 
 temp = string(split(ROOTPATH, "\"));
@@ -39,6 +38,7 @@ print(FigB2, strcat(BPATH, "BEHAVIOR_IRREG"), "-djpeg", "-r300");
 print(FigB3, strcat(BPATH, "BEHAVIOR_PT"), "-djpeg", "-r300");
 
 %% Passive 3
+window = [0, 2000];
 pID = 3;
 trialAll = trialDatasets(pID).trialAll;
 EEGDataset = EEGDatasets(pID);
@@ -83,6 +83,7 @@ for index = 1:length(ICIs)
 end
 
 %% Active 1
+window = [0, 2000];
 pID = 4;
 trialAll = trialDatasets(pID).trialAll;
 EEGDataset = EEGDatasets(pID);
@@ -124,4 +125,79 @@ for index = 1:length(ICIs)
     setAxes(Fig, "Visible", "off");
     plotLayoutEEG(Fig, 0.3);
     print(Fig, strcat(FIGPATH, "Irreg_", strrep(num2str(ICIs(index)), '.', '_')), "-djpeg", "-r300");
+end
+
+%% Active 2
+window = [0, 2600];
+pID = 5;
+trialAll = trialDatasets(pID).trialAll;
+EEGDataset = EEGDatasets(pID);
+ICIs = unique([trialAll.ICI]);
+FIGPATH = strcat("..\Figs\", dateStr, "\Active2\");
+mkdir(FIGPATH);
+
+% Reg
+for index = 1:length(ICIs)
+    trials = trialAll([trialAll.ICI] == ICIs(index) & [trialAll.correct] & ([trialAll.type] == "REG" | ([trialAll.type] == "PT" & [trialAll.freq] ~= 250)));
+
+    if isempty(trials)
+        continue;
+    end
+
+    [~, chMean, ~] = selectEEG(EEGDataset, trials, window, th);
+    legendStr = strcat("active 2 | REG ", string(num2str(ICIs(index))));
+    Fig = plotRawWaveEEG(chMean, [], window, 1600, legendStr);
+    scaleAxes(Fig, "y", [-5, 5]);
+    scaleAxes(Fig, "x", [0, 2600]);
+    setAxes(Fig, "Visible", "off");
+    plotLayoutEEG(Fig, 0.3);
+    print(Fig, strcat(FIGPATH, "Reg_", strrep(num2str(ICIs(index)), '.', '_')), "-djpeg", "-r300");
+end
+
+% Irreg
+for index = 1:length(ICIs)
+    trials = trialAll([trialAll.ICI] == ICIs(index) & ~[trialAll.miss] & [trialAll.type] == "IRREG");
+
+    if isempty(trials)
+        continue;
+    end
+
+    [~, chMean, ~] = selectEEG(EEGDataset, trials, window, th);
+    legendStr = strcat("active 2 | IRREG", string(num2str(ICIs(index))));
+    Fig = plotRawWaveEEG(chMean, [], window, 1600, legendStr);
+    scaleAxes(Fig, "y", [-5, 5]);
+    scaleAxes(Fig, "x", [0, 2600]);
+    setAxes(Fig, "Visible", "off");
+    plotLayoutEEG(Fig, 0.3);
+    print(Fig, strcat(FIGPATH, "Irreg_", strrep(num2str(ICIs(index)), '.', '_')), "-djpeg", "-r300");
+end
+
+%% Active compare
+window1 = [1000, 2000];
+trialAll1 = trialDatasets(4).trialAll;
+EEGDataset1 = EEGDatasets(4);
+
+window2 = [1600, 2600];
+trialAll2 = trialDatasets(5).trialAll;
+EEGDataset2 = EEGDatasets(5);
+
+ICIs = unique([trialAll1.ICI]);
+ICIs(ICIs == 0 | ICIs == 8) = [];
+FIGPATH = strcat("..\Figs\", dateStr, "\Active1&2 compare\");
+mkdir(FIGPATH);
+
+% Reg
+for index = 1:length(ICIs)
+    trials1 = trialAll1([trialAll1.ICI] == ICIs(index) & [trialAll1.correct] & [trialAll1.type] == "REG");
+    trials2 = trialAll2([trialAll2.ICI] == ICIs(index) & [trialAll2.correct] & [trialAll2.type] == "REG");
+    [~, chData(1).chMean, ~] = selectEEG(EEGDataset1, trials1, window1, th);
+    [~, chData(2).chMean, ~] = selectEEG(EEGDataset2, trials2, window2, th);
+    chData(1).color = "k";
+    chData(2).color = "r";
+    Fig = plotRawWaveMultiEEG(chData, [0, 1000], [], num2str(ICIs(index)));
+    scaleAxes(Fig, "y", [-5, 5]);
+    scaleAxes(Fig, "x", [0, 500]);
+%     setAxes(Fig, "Visible", "off");
+    plotLayoutEEG(Fig, 0.3);
+%     print(Fig, strcat(FIGPATH, "Reg_", strrep(num2str(ICIs(index)), '.', '_')), "-djpeg", "-r300");
 end
--- utils/excludeTrials.m
+++ utils/excludeTrials.m
@@ -4,7 +4,13 @@ function trialsEEG = excludeTrials(trialsEEG, th)
     chStd = cell2mat(cellfun(@std, temp, "UniformOutput", false));
     idx = cellfun(@(x) sum(x > chMean + 3 * chStd | x < chMean - 3 * chStd, 2) / size(x, 2), trialsEEG, "UniformOutput", false);
     idx = cellfun(@(x) ~any(x > th), idx);
-    disp(['Trial ', num2str(find(~idx)'), ' are excluded.']);
+
+    if ~isempty(find(~idx, 1))
+        disp(['Trial ', num2str(find(~idx)'), ' are excluded.']);
+    else
+        disp('All pass');
+    end
+
     trialsEEG = trialsEEG(idx);
     return;
 end
