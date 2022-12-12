7a44ec05fbd8d0a6a653f5866fbf94f5292a0086
TOMORI233 <ihatewo@qq.com>
2022-12-12 20:54:13 +0800

    update 20221212 by xhx

--- stimuli/utils/Human MRI/generateRandTimeSeq.m
+++ stimuli/utils/Human MRI/generateRandTimeSeq.m
@@ -1,15 +1,15 @@
 function x = generateRandTimeSeq(durTotal, N, randRange)
 %     durTotal = 10; % sec
-%     N = 10; % sec
-%     randRange = 0.5:0.05:1.5; % sec
+%     N = 20; % sec
+%     randRange = 0.3:0.05:0.7; % sec
     x = zeros(N, 1);
 
-    for i = 1:10
+    for i = 1:N
         x(i) = randRange(randperm(length(randRange), 1));
     end
     
     while sum(x) ~= durTotal
-        for i = 1:10
+        for i = 1:N
             x(i) = randRange(randperm(length(randRange), 1));
         end
     end
