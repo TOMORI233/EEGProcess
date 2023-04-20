57aed225fd4265fea3ab6c73a22d1120dbd76c02
TOMORI233 <ihatewo@qq.com>
2023-04-20 20:00:54 +0800

    annotation and file path correction

--- config/preprocessConfig.m
+++ config/preprocessConfig.m
@@ -2,6 +2,6 @@ function optsDefault = preprocessConfig()
     optsDefault.rules = rulesConfig();
     optsDefault.fhp = 0.5;
     optsDefault.flp = 40;
-    optsDefault.DATEStr = [];
+    optsDefault.DATEStr = []; % To specify a rules_[DATE].xlsx file for this DATE
     return;
 end
--- config/rulesConfig.m
+++ config/rulesConfig.m
@@ -2,7 +2,7 @@ function rules = rulesConfig(TABLEPATH)
     narginchk(0, 1);
 
     if nargin < 1
-        TABLEPATH = "rules\rules.xlsx";
+        TABLEPATH = fullfile(getRootDirPath(fullfile(fileparts(mfilename("fullpath"))), 1), "rules", "rules.xlsx");
     end
 
     try
