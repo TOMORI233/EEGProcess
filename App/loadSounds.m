61c492b1aa8c51dde5ac88130322b04dd4781746
TOMORI233 <ihatewo@qq.com>
2023-05-08 09:58:29 +0800

    update 20230508 by xhx ROG

--- App/loadSounds.m
+++ App/loadSounds.m
@@ -1,7 +1,7 @@
 function [sounds, soundNames, fs, controlIdx] = loadSounds(pID)
-    folders = dir('sounds\');
+    folders = dir(fullfile(fileparts(mfilename("fullpath")), 'sounds'));
     targetFolder = folders(arrayfun(@(x) isequal(str2double(x.name), pID), folders));
-    files = dir(fullfile('sounds\', targetFolder.name));
+    files = dir(fullfile(fileparts(mfilename("fullpath")), 'sounds', targetFolder.name));
     [~, soundNames, exts] = cellfun(@(x) fileparts(x), {files.name}, "UniformOutput", false);
     soundPaths = arrayfun(@(x) fullfile(x.folder, x.name), files(strcmp(exts, '.wav')), "UniformOutput", false);
     soundNames = soundNames(3:end)';
